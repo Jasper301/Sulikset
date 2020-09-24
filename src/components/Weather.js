c0aa1b605026e6eb572ba198ad9b209f567ebd3f
Jasper301 <69836078+Jasper301@users.noreply.github.com>
2020-09-24 10:12:44 +0300

    24.9.2020

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -13,7 +13,7 @@ function Weather() {
   const initWeather = [];
   const [weather, setWeather] = useState(initWeather);
 
-  // hakee kosetus/lämpötila taulukon.
+  // hakee kosteus/lämpötila taulukon.
   fetch('https://funcvariaiot.azurewebsites.net/api/HttpTriggerGetIotData?code=qO5qkShg0osHqY0BB2nfXI/anPgQ/K/3mIF7VTCFfaTdrvo6wl6DKw==&amount=50')
     .then(response => response.json())
     .then(json => setWeather([...json]));
@@ -22,14 +22,16 @@ function Weather() {
   let chartHumData = [];
 
   const rows = () => weather.slice(0, 23).reverse().map(temphum => {
+    // Poistaa ylimääräisiä merkintöjä.
     const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] + '.' + temphum.PublishedAt.split('T')[0].split('-')[1] + '.' + temphum.PublishedAt.split('T')[0].split('-')[0]
     const measurementTime = temphum.PublishedAt.split('T')[1].split(':')[0] + ':' + temphum.PublishedAt.split('T')[1].split(':')[1]
     chartTempData.push({ x: String(measurementTime), y: parseInt(temphum.Temp) });
     chartHumData.push({ x: String(measurementTime), y: parseInt(temphum.Temp), label: String(temphum.Temp.split('.')[0] + "%") });
     return <div key={humtempkey++}><d>Pvm:</d> {measurementDate}, <b>klo:</b> {measurementTime}---------------<b>Ilmankosteus:</b> {temphum.Hum.split('.')[0]}%-------------<b>Lämpötila:</b> {temphum.Temp.split('.')[0]}°C</div>
   })
+  // Lämpötila taulukko
   const TempData = chartTempData;
-
+  // Kosteus taulukko
   const HumData = chartHumData;
 
   return (
@@ -51,8 +53,11 @@ function Weather() {
 
 
       <VictoryChart
+      //Ilmankosteus taulukon koko.
         domainPadding={{ x: 15, y: 50 }}
+        // Ilmankosteus palkkien leveys.
         width={1400}
+        //Ilmankosteus palkkien korkeus.
         height={350}>
         <VictoryBar
           containerComponent={<VictoryVoronoiContainer />}
@@ -68,13 +73,17 @@ function Weather() {
 
       <h1>Lämpötila </h1>
       <VictoryChart
+      // Läpmpötila taulukon koko.
         domainPadding={{ x: 15, y: 50 }}
+        // Lämpötila pisteiden leveys.
         width={1400}
+        // Lämpötila pisteiden korkeus.
         height={350}
         containerComponent={<VictoryVoronoiContainer />}
       >
 
         <VictoryGroup
+          // vaihtaa väriä
           color="#c43a31"
           labels={({ datum }) => `${datum.y} °C`}
           labelComponent={
